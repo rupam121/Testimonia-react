9bec17502fa61557a620709ab9f9929185ccdb0a
RUPAM GIRI <rupamgiri4321@gmail.com>
2024-09-10 15:18:30 +0530

    image error fix

--- src/App.js
+++ src/App.js
@@ -14,20 +14,20 @@ function App() {
   return (
     <div className="w-3/4 m-auto">
       <div className="mt-20">
-      <Slider {...settings}>
-        {data.map((d) => (
-          <div className="bg-white h-[450px] text-black rounded-xl">
+        <Slider {...settings}>
+          {data.map((d) => (
+            <div className="bg-white h-[450px] text-black rounded-xl">
 
-            <div className="h-56 rounded-t-xl bg-green-500 flex justify-center items-center">
-              <img src={d.img} alt="" className="h-44 w-44 rounded-full"/>
+              <div className="h-56 rounded-t-xl bg-green-500 flex justify-center items-center">
+                <img src={`${process.env.PUBLIC_URL}/${d.img}`} alt="" className="h-44 w-44 rounded-full" />
+              </div>
+              <div className="flex flex-col justify-center items-center gap-4 p-4">
+                <p className="text-xl font-semibold">{d.name}</p>
+                <p>{d.review}</p>
+                <button className="bg-green-500 text-white text-lg px-6 py-1 rounded-xl">Read More</button>
+              </div>
             </div>
-            <div className="flex flex-col justify-center items-center gap-4 p-4">
-              <p className="text-xl font-semibold">{d.name}</p>
-              <p>{d.review}</p>
-              <button className="bg-green-500 text-white text-lg px-6 py-1 rounded-xl">Read More</button>
-            </div>
-          </div>
-        ))}
+          ))}
         </Slider>
       </div>
     </div>
@@ -37,35 +37,17 @@ function App() {
 const data = [
   {
     name: `Raj Kumar`,
-    img: `../public/students/student3.png`,
+    img: `students/student3.png`,
+    review: `lorem ipsome is there we all are here to study by the way we are engineer.`
+  }, {
+    name: `Raj Kumar`,
+    img: `students/student3.png`,
+    review: `lorem ipsome is there we all are here to study by the way we are engineer.`
+  }, {
+    name: `Raj Kumar`,
+    img: `students/student3.png`,
     review: `lorem ipsome is there we all are here to study by the way we are engineer.`
   },
-  {
-    name: `Raj Kumar`,
-    img: `/students/student3.png`,
-    review: `lorem ipsome is there we all are here to study by the way we are engineer.`
-  }, {
-    name: `Raj Kumar`,
-    img: `/students/student3.png`,
-    review: `lorem ipsome is there we all are here to study by the way we are engineer.`
-  }, {
-    name: `Raj Kumar`,
-    img: `/students/student3.png`,
-    review: `lorem ipsome is there we all are here to study by the way we are engineer.`
-  }, {
-    name: `Raj Kumar`,
-    img: `/students/student3.png`,
-    review: `lorem ipsome is there we all are here to study by the way we are engineer.`
-  }, {
-    name: `Raj Kumar`,
-    img: `/students/student3.png`,
-    review: `lorem ipsome is there we all are here to study by the way we are engineer.`
-  }, {
-    name: `Raj Kumar`,
-    img: `/students/student3.png`,
-    review: `lorem ipsome is there we all are here to study by the way we are engineer.`
-  }
-
 ]
 
 export default App;
